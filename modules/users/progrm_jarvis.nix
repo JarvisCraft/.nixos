e511bce9fd571d70a3ef4d8fb452cfd0263ce1bf
Petr Portnov <mrjarviscraft@gmail.com>
2024-11-10 06:15:05 +0300

    feat(users/progrm_jarvis): add authorized SSH key

--- modules/users/progrm_jarvis.nix
+++ modules/users/progrm_jarvis.nix
@@ -14,6 +14,9 @@
       prismlauncher
     ];
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqwhIGYMJynS7sLGUhV8Q0lD4j8tjUMBP9oXUHx3g90 openpgp:0x7C00EA6A"
+    ];
   };
 
   programs.git.config.user = {
