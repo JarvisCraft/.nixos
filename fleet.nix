5aeda52bbabe595fec78a64e99fe52d7b2a8f334
Petr Portnov <mrjarviscraft@gmail.com>
2025-10-03 02:12:34 +0300

    chore(deps): update on 03.10.2025

--- fleet.nix
+++ fleet.nix
@@ -1,9 +1,10 @@
 # This file contains fleet state and shouldn't be edited by hand
 
-{version = "0.1.0";
-gcRootPrefix = "fleet-gc-YqamBlht";
-managerKeys = [ ];
-hosts = { };
+{
+  version = "0.1.0";
+  gcRootPrefix = "fleet-gc-YqamBlht";
+  managerKeys = [ ];
+  hosts = { };
 }
 
 # vim: ts=2 et nowrap
