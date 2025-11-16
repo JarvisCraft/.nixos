c4dac88b55cf8fc368f3471d1387b70dac3a61a5
Petr Portnov <mrjarviscraft@gmail.com>
2025-11-16 21:58:34 +0300

    build(flake): use `fleet@master` again

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     fleet = {
-      url = "github:deltarocks/fleet/stable-rust";
+      url = "github:deltarocks/fleet";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
