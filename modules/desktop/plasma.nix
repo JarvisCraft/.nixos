e54621ec730a572744ecfd90a7559fb2889ebf3c
Petr Portnov <mrjarviscraft@gmail.com>
2026-05-21 18:23:39 +0300

    feat(modules/desktop): add Gruvbox theme

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -1,3 +1,11 @@
+{ pkgs, ... }:
 {
   services.desktopManager.plasma6.enable = true;
+  environment.systemPackages = with pkgs; [
+    kde-gruvbox
+    gruvbox-plus-icons
+    gruvbox-gtk-theme
+    gruvbox-kvantum
+    capitaine-cursors-themed
+  ];
 }
